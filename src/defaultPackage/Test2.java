a99af7b612005e726bade9713476bf559528f0ad
User <User@192.168.1.6>
2024-06-06 14:15:39 +0100

    commiting Test 2 to Branch 1

---
+++ src/defaultPackage/Test2.java
@@ -0,0 +1,18 @@
+package defaultPackage;
+
+public class Test2 {
+	
+	
+	public static void main(String[] args) {
+	String hungry = "yes" ;
+	
+	
+	if(hungry == "yes") {
+		System.out.println("eat");
+	}
+	else {
+		System.out.println("do not eat");
+	}
+		
+	
+}}
